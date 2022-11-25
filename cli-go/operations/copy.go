b10f97561f75baf3890b68eefe7ac50fcce7d6ae
Assaf Vayner <assafvayner@gmail.com>
2022-11-25 14:10:58 -0800

    go based cli tool implementation

---
+++ cli-go/operations/copy.go
@@ -0,0 +1,38 @@
+package operations
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"gitlab.cs.washington.edu/assafv/fs3/cli-go/utils"
+	fs3 "gitlab.cs.washington.edu/assafv/fs3/protos/fs3"
+)
+
+func Copy(localFile *os.File, remotePath string) {
+	fileContents, err := os.ReadFile(localFile.Name())
+	localFile.Close()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	client := utils.GetFs3Client()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req := &fs3.CopyRequest{
+		FilePath:    remotePath,
+		FileContent: fileContents,
+	}
+
+	reply, err := client.Copy(ctx, req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+  utils.CheckFilePaths(remotePath, reply.GetFilePath())
+  utils.CheckStatus(reply.GetStatus())
+}
---
+++ cli-go/operations/remove.go
@@ -0,0 +1,30 @@
+package operations
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"gitlab.cs.washington.edu/assafv/fs3/cli-go/utils"
+	fs3 "gitlab.cs.washington.edu/assafv/fs3/protos/fs3"
+)
+
+func Remove(remotePath string) {
+	client := utils.GetFs3Client()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req := &fs3.RemoveRequest{
+		FilePath: remotePath,
+	}
+
+	reply, err := client.Remove(ctx, req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	utils.CheckFilePaths(remotePath, reply.GetFilePath())
+	utils.CheckStatus(reply.GetStatus())
+}
